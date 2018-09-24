c1d816bcf31877bfb0c4e30fd7526b97c4ce82ae
soda <soda@brave-law.lan>
2018-09-24 12:58:08 -0700

    Release v2.1.0 (WIP) - "Farmers for hire" feature partially added

--- src/runner/CowSim.java
+++ src/runner/CowSim.java
@@ -7,7 +7,7 @@ package runner;
 */
 
 /** Ideas 
- * Use  MouseInfo.getPointerInfo().getLocation() to get mouse X and Y position
+ * Use MouseInfo.getPointerInfo().getLocation() to get mouse X and Y position
 */
 
 /** Bugs */
